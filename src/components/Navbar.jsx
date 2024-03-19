fb92223d1de09406a36eb3f61eabc2ba0dfb3690
sanh <s0165671254@gmail.com>
2024-03-19 12:30:51 +0700

    Your commit message here

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import './Navbar.css';
+import { assets } from "../assets/assets";
 
 const Navbar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState("home")
     return (
         <div className="navbar">
-            <img src="/path/to/logo.png" alt="Logo" className="navbar-logo" />
+            <img src={assets.logo} alt="Logo" className="navbar-logo" />
             <ul className="navbar-menu">
                 <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</li>
                 <li onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu </li>
@@ -14,9 +15,9 @@ const Navbar = ({ setShowLogin }) => {
                 <li onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}> contact us</li>
             </ul>
             <div className="navbar-right">
-                <img src="/path/to/logo.png" alt="Logo" />
+                <img src={assets.search_icon} alt="Logo" />
                 <div className="navbar-seacrh-icon">
-                    <img src="/path/to/logo.png" alt="Logo" />
+                    <img src={assets.basket_icon} alt="Logo" />
                     <div className="dot"></div>
                 </div>
                 <button onClick={() => setShowLogin(true)}>Sign In</button>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css"
+import { StoreContext } from "../../context/StoreContext";
 
 const Cart = () => {
+    const { cartItems, food_list, removeFormCart } = useContext(StoreContext)
     return (
-        <div>
+        <div className="cart">
+            <div className="cart-items-title">
+                <p>Item</p>
+                <p>title</p>
+                <p> price</p>
+                <p> Quantity</p>
+                <p>Total</p>
+                <p> Remove</p>
+            </div>
 
         </div>
     )
